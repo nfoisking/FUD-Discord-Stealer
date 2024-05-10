7710308b6d8031203c5dae9b56f3f33156cb6ed1
nfo <ryderfoda1@gmail.com>
2024-05-10 19:44:15 -0300

    Update api.py

--- api-dual/api.py
+++ api-dual/api.py
@@ -8,12 +8,12 @@ url = "YOUR_SHIT_WEBHOOK_API"
 @app.route('/ilovedicklol', methods=['POST'])
 def proxy_discord_webhook():
     if not request.json:
-        return jsonify({"error": "invalid webhook lol"}), 400
+        return jsonify({"error": "Invalid Webhook"}), 400
     response = requests.post(url, json=request.json)
     if response.status_code == 204:
-        return jsonify({"message": "sended"}), 200
+        return jsonify({"message": "Webhook Sended"}), 200
     else:
-        return jsonify({"error": "idk"}), 500
+        return jsonify({"error": "?"}), 500
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 0123)
